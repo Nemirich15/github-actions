e2232060a395f281f4425d9f67de1034cecc8c64
nemirich <nemirka15@gmail.com>
2025-01-27 22:27:04 +0300

    bugs fixed

--- test/pages/password.page.js
+++ test/pages/password.page.js
@@ -2,13 +2,13 @@ import Page from "./page";
 
 class Password extends Page {
 
-    get current() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[1]/div/input')};
-    get new() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[3]/input')};
-    get retype() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[4]/input')};
-    get fogot() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[2]/a')};
-    get save() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/button')};
-    get policy() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/div[2]/span/a[1]')};
-    get terms() {return ('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/div[2]/span/a[2]')};
+    get current() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[1]/div/input')};
+    get new() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[3]/input')};
+    get retype() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[4]/input')};
+    get fogot() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/div[2]/a')};
+    get save() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/form/button')};
+    get policy() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/div[2]/span/a[1]')};
+    get terms() {return $('//*[@id="main-content"]/div/div[5]/div[2]/div[2]/div[2]/span/a[2]')};
 
 
     open() {
@@ -20,7 +20,7 @@ class Password extends Page {
     };
 
     new_pass() {
-        this.new.set.setValue('15nemira15')
+        this.new.setValue('15nemira15')
     };
 
     retype_pass() {
