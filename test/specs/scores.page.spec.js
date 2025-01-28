ff6934fd1079b72ce3dd7fdfbeb94936f97de193
nemirich <nemirka15@gmail.com>
2025-01-28 23:16:01 +0300

    bugs fexed

--- test/specs/scores.page.spec.js
+++ test/specs/scores.page.spec.js
@@ -3,26 +3,33 @@ import Scores from "../pages/scores.page";
 describe('Scores page', () => {
 
     it('Open scores', async() => {
-        Scores.open()
+        Scores.open();
+        await browser.pause(2000);
     });
 
     it('Enter date', async () => {
-        Scores.click_date()
+        Scores.click_date();
+        await browser.pause(2000);
     });
 
     it('Set time', async () => {
-        Scores.set_time()
+        Scores.set_time();
+        await browser.pause(2000);
     });
 
     it('Click date before', async () => {
-        Scores.date_before_weak()
+        Scores.date_before_weak();
+        await browser.pause(2000);
+
     });
 
     it('Click date after', async () => {
-        Scores.date_next_weak()
+        Scores.date_next_weak();
+        await browser.pause(2000);
     });
 
     it('Time choise', async () => {
-        Scores.choise_time()
+        Scores.choise_time();
+        await browser.pause(2000);
     });
 })
